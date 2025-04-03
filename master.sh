d83cc56b12d6c27568aa734baf01ea14127da9e5
dtunguz <durdijatunguz@gmail.com>
2025-04-03 11:01:31 +0200

    Update master.sh

--- master.sh
+++ master.sh
@@ -33,7 +33,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key \
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' \
 |  tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+apt-get install -y kubeadm=1.32.1-1.1 kubelet=1.32.1-1.1 kubectl=1.32.1-1.1
 apt-mark hold kubelet kubeadm kubectl
 hostname -i
 my_var=$(ifconfig enX0 | grep "inet " | awk -F'[: ]+' '{ print $3 }')
